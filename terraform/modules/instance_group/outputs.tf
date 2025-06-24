68424da4ad7bc458a75b2554384f8d4325ed9020
root <root@instance-20250617-062200.asia-east1-c.c.silver-impulse-462505-s4.internal>
2025-06-24 14:00:36 +0800

    fix outputs.tf

--- terraform/modules/instance_group/outputs.tf
+++ terraform/modules/instance_group/outputs.tf
@@ -1,3 +1,6 @@
+output "instance_template_self_link" {
+  value = google_compute_instance_template.template.self_link
+}
 output "instance_group_name" {
   value = google_compute_region_instance_group_manager.mig.name
 }
