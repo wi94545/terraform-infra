fd70b5a1420220973129203f3e27852b976bd576
root <root@instance-20250617-062200.asia-east1-c.c.silver-impulse-462505-s4.internal>
2025-06-24 13:17:28 +0800

    fix autoscaler

--- terraform/modules/instance_group/main.tf
+++ terraform/modules/instance_group/main.tf
@@ -35,7 +35,6 @@ resource "google_compute_region_instance_group_manager" "mig" {
 
 resource "google_compute_autoscaler" "as" {
   name   = "autoscaler-${var.group_name}"
-  region = var.region
   target = google_compute_region_instance_group_manager.mig.self_link
 
   autoscaling_policy {
